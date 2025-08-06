bf5f0d51b7ad0411cd921011e385bb0b2935f523
ulisesrey <ulises.rey.torne@gmail.com>
2025-08-06 15:46:30 +0200

    refractor

--- pitch_detector/contour.py
+++ pitch_detector/contour.py
@@ -6,25 +6,11 @@ import scipy.fft
 import numpy as np
 import librosa
 import librosa.display
+import pandas as pd
+import os
 
-
-filename = 'data/raw/oddcast-b07f2d6d-6b666bb1-8f934c9f-8e8628ec-19bf6b1e'
-filename = filename + ".wav"
-data, fs = librosa.load(filename, sr=None)
-
-datalength = len(data)
-time = np.arange(datalength)/fs
-
-plt.plot(time, data)
-plt.show()
-
-# Typically fundamental frequencies lie roughly in the range 80 to 450 Hz
-fmin = 80
-fmax = 450
-frame_length = 1024
-hop_length = 256  # or 512
-
-f0pyin, voiced_flag, voiced_prob = librosa.pyin(data.astype(float), 
+def plot_f0(data, fmin, fmax, frame_length, hop_length, label=None):
+    f0pyin, voiced_flag, voiced_prob = librosa.pyin(data.astype(float), 
                                     sr = fs, # sampling frequency
                                     fmin=fmin, 
                                     fmax=fmax, 
@@ -33,21 +19,46 @@ f0pyin, voiced_flag, voiced_prob = librosa.pyin(data.astype(float),
 
 
 
-# Convert frame indices to time (in seconds)
-times = librosa.frames_to_time(np.arange(len(f0pyin)), sr=fs, hop_length=hop_length)
+    # Convert frame indices to time (in seconds)
+    times = librosa.frames_to_time(np.arange(len(f0pyin)), sr=fs, hop_length=hop_length)
 
-# Plot
-plt.figure(figsize=(10, 4))
-plt.plot(times, f0pyin, label='Estimated F₀ (pyin)', color='blue')
+    # Plot
+    #plt.figure(figsize=(10, 4))
+    plt.plot(times, f0pyin, label=label, color='blue')
 
-# Optionally overlay voicing probability
-# plt.plot(times, voiced_prob * f0_range_Hz[1], label='Voiced Probability (scaled)', alpha=0.5)
+    # Optionally overlay voicing probability
+    # plt.plot(times, voiced_prob * f0_range_Hz[1], label='Voiced Probability (scaled)', alpha=0.5)
 
-# Formatting
-plt.xlabel('Time (s)')
-plt.ylabel('Fundamental Frequency (Hz)')
-plt.title('F₀ estimation using librosa.pyin')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    # Formatting
+    plt.xlabel('Time (s)')
+    plt.ylabel('Fundamental Frequency (Hz)')
+    plt.title('F₀ estimation using librosa.pyin')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    #plt.show()
+
+
+if __name__ == "__main__":
+    filename = 'data/raw/oddcast-b07f2d6d-6b666bb1-8f934c9f-8e8628ec-19bf6b1e'
+    filename = filename + ".wav"
+    data, fs = librosa.load(filename, sr=None)
+    file = data
+    fmin = 50
+    fmax = 450
+    frame_length = 1024
+    hop_length = 256  # or 512
+    df = pd.read_csv("data/raw/basic_chinese_characters_ankicard.csv", header=0)
+    # format agnostic path
+    df["sound_path"] = df["mp3_path"].str.removesuffix(".mp3")
+
+    # Fitler for character
+    filtered_df = df[df["Pinyin"].str.contains("ǎ", na=False)]
+
+    sound_data_root = "data/sounds"
+    for index, row in filtered_df.iterrows():
+        filepath = os.path.join(sound_data_root, row["sound_path"]+".wav")
+        print(filepath)
+        data, fs = librosa.load(filepath,sr=None)
+        plot_f0(data, fmin, fmax, frame_length, hop_length)
+    plt.show()
