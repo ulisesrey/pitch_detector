dbdc753829621a37f0f63006c04e033bfc9593b3
ulisesrey <ulises.rey.torne@gmail.com>
2025-08-07 16:56:40 +0200

    contour.py module calculates and saves f0 and associated params

--- pitch_detector/contour.py
+++ pitch_detector/contour.py
@@ -1,13 +1,53 @@
 # Initialization
-import matplotlib.pyplot as plt
-import numpy as np
 import librosa
 import pandas as pd
 import os
-from tones import TONE_DICT
-from plots import plot_f0
+from tqdm import tqdm
+import numpy as np
 
 # TODO: Will contain the F0 detection code
+def compute_f0(data, fs, fmin, fmax, frame_length, hop_length):
+    """Compute F0 Frequency using PYIN algorithm"""
+    f0pyin, voiced_flag, voiced_prob = librosa.pyin(data.astype(float), 
+                                    sr = fs, # sampling frequency
+                                    fmin=fmin, 
+                                    fmax=fmax, 
+                                    frame_length=frame_length, 
+                                    hop_length=hop_length) 
 
+    # Convert frame indices to time (in seconds)
+    times = librosa.frames_to_time(np.arange(len(f0pyin)), sr=fs, hop_length=hop_length)
+
+    return f0pyin, voiced_flag, voiced_prob, times
+
+    
 if __name__ == "__main__":
-    # Parameters
+    fmin = 50
+    fmax = 450
+    frame_length = 1024
+    hop_length = 256  # or 512
+
+    df = pd.read_csv("data/raw/basic_chinese_characters_ankicard.csv", index_col=0, header=0)
+    save_path = "data/processed/f0_values.npz"
+    if os.path.exists(save_path):
+         print(f"Save Path already exists in {save_path}!")
+    for index, row in tqdm(df.iterrows()):
+        wav_filepath = row["audio_full_path"]
+        # try if file exists
+        try:
+            os.path.exists(wav_filepath)
+            data, fs = librosa.load(wav_filepath, sr=None)
+            f0pyin, voiced_flag, voiced_prob, times = compute_f0(data,
+                                                                fs,
+                                                                fmin,
+                                                                fmax,
+                                                                frame_length,
+                                                                hop_length)
+            np.savez(save_path,index=index,
+                audio_path=wav_filepath,
+                f0pyin=f0pyin,
+                voiced_flag=voiced_flag,
+                voiced_prob=voiced_prob,
+                times=times)
+        except Exception as e:
+                print(f"⚠️ Error processing {wav_filepath}: {e}")
