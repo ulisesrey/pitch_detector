341b10ea915e918282ed6b3d2c119c24a6b3f090
ulisesrey <ulises.rey.torne@gmail.com>
2025-08-07 17:40:30 +0200

    preparing proper way to save data

--- pitch_detector/contour.py
+++ pitch_detector/contour.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
 
     df = pd.read_csv("data/raw/basic_chinese_characters_ankicard.csv", index_col=0, header=0)
     save_path = "data/processed/f0_values.npz"
+    f0_data = {}
     if os.path.exists(save_path):
          print(f"Save Path already exists in {save_path}!")
     for index, row in tqdm(df.iterrows()):
@@ -43,11 +44,12 @@ if __name__ == "__main__":
                                                                 fmax,
                                                                 frame_length,
                                                                 hop_length)
-            np.savez(save_path,index=index,
-                audio_path=wav_filepath,
-                f0pyin=f0pyin,
-                voiced_flag=voiced_flag,
-                voiced_prob=voiced_prob,
-                times=times)
+            f0_data[f"{index}_f0pyin"] = f0pyin
+            f0_data[f"{index}_voiced_flag"] = voiced_flag
+            f0_data[f"{index}_voiced_prob"] = voiced_prob
+            f0_data[f"{index}_times"] = times
         except Exception as e:
                 print(f"⚠️ Error processing {wav_filepath}: {e}")
+
+    # Save all in one compressed .npz file
+    np.savez_compressed(save_path, **f0_data)
