10adaa3b8bcd831328d3932f63ad43975cecb5da
ulisesrey <ulises.rey.torne@gmail.com>
2025-08-07 13:58:47 +0200

    add plt.show()

--- pitch_detector/plots.py
+++ pitch_detector/plots.py
@@ -6,9 +6,10 @@ import pandas as pd
 def plot_tone_distribution(df):
     """Plot tone distribution"""
     df["tone"].value_counts().plot.bar()
-    
+
 
 
 if __name__ == "__main__":
     df = pd.read_csv("data/raw/basic_chinese_characters_ankicard.csv", index_col=0, header=0)
     plot_tone_distribution(df)
+    plt.show()
